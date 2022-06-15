f964d76b540e7e65d1e78329cdc0b5e70dc997b6
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-06-15 16:27:41 +0300

    hero layout

---
+++ components/hero-layout.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const bigBoxVariants = {
+    hidden: {
+        x:-350,
+    },
+    visible: {
+        x: 0,
+        transition: {
+            delay: 0.5,
+            
+            when: "beforeChildren",
+            ease: 'easeOut',
+            staggerChildren: .4,
+        }
+    }
+}
+
+const listVariants = {
+    hidden: {
+        y: 50,
+        opacity: 0,
+        
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            ease: 'easeOut',
+            duration: 1.5
+        }
+        
+       
+    }
+
+}
+
+const Box = () => {
+  return (
+      <motion.ul 
+      className="bigBox"
+      variants={bigBoxVariants}
+      initial="hidden"
+      animate="visible"
+      >
+
+   {/* {[1,2,3].map((box, index)=> {
+       return <motion.li key={index}
+       className='bg-blue-200 w-32 h-32 m-3'
+       variants={listVariants}
+       ></motion.li>
+    })} */}
+<motion.li 
+       className='font-black text-7xl mb-14'
+       variants={listVariants}
+       ><h1>Išamnūs it sprendimai < br/>tiesiai į Jūsų namus</h1></motion.li>
+<motion.li 
+       className='mb-20'
+       variants={listVariants}
+       > <h3 className='mb-6'>Kvieciame užsirašyti konsultacijai apie jums rūpimus sprendimus <br/> Kvieciame užsirašyti konsultacijai apie jums rūpimus sprendimus</h3>
+           <button className=' text-xl font-black bg-blue-200 px-8 py-3'>Batonas</button></motion.li>
+<motion.li 
+       className=''
+       variants={listVariants}
+       ><div className='bg-green-600 h-80 w-full'></div></motion.li>
+
+    </motion.ul>
+  )
+}
+
+export default Box
--- components/sections/hero.jsx
+++ components/sections/hero.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import HeroLayout from '../hero-layout';
 
 const Hero = () => {
   return (
     <section className=' one section pt-[58px] flex justify-center'>
-        <div className='container margin-auto bg-yellow-200 h-full'></div>
+        <div className='container margin-auto bg-yellow-200 h-full w-full flex justify-center items-center text-center'>
+            <HeroLayout />
+        </div>
     </section>
   )
 }
